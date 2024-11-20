9cf693ac35e8d829c611b7a85dc7bd205b02ac99
raistlin-hess <raistlin-hess>
2024-11-19 21:11:19 -0500

    Setting up ssl for registration

--- tools/docker/char-registration/src/env.js
+++ tools/docker/char-registration/src/env.js
@@ -1,6 +1,8 @@
 //Define all env variables here
 const ENV = {
-  EXPRESS_PORT: Number(process.env.EXPRESS_PORT) || 80,
+  EXPRESS_PORT: Number(process.env.EXPRESS_PORT) || 443,
+  EXPRESS_CERT_PATH: process.env.EXPRESS_CERT_PATH ?? 'cert.pem',
+  EXPRESS_CERT_PRIV_KEY_PATH: process.env.EXPRESS_CERT_PRIV_KEY_PATH ?? 'privkey.pem',
   DB_HOST: process.env.DB_HOST ?? 'localhost',
   DB_USER: process.env.DB_USER ?? 'ragnarok',
   DB_PASSWORD: process.env.DB_PASSWORD ?? 'ragnarok',
--- tools/docker/char-registration/src/index.js
+++ tools/docker/char-registration/src/index.js
@@ -1,6 +1,9 @@
+const https = require('https');
 const express = require('express');
+const { readFileSync } = require('fs');
+const { resolve } = require('path');
 const { initializeConnectionPool } = require('./server/database');
-const { EXPRESS_PORT } = require('./env');
+const { EXPRESS_PORT, EXPRESS_CERT_PATH, EXPRESS_CERT_PRIV_KEY_PATH } = require('./env');
 
 
 //Create and configure server
@@ -9,6 +12,10 @@ const app = express();
 app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
+
+//Prepare SSL
+const cert = readFileSync(resolve(EXPRESS_CERT_PATH));
+const privateKey = readFileSync(resolve(EXPRESS_CERT_PRIV_KEY_PATH));
 
 //Routing
 console.log('Registering route handlers...');
@@ -19,7 +26,11 @@ console.log('Initializing database connection pool...');
 initializeConnectionPool();
 console.log('Connection pool initialized');
 
-
 //Start server
-app.listen(EXPRESS_PORT);
-console.log(`Server listening on port: ${EXPRESS_PORT}`);
+https.createServer({
+  key: privateKey,
+  cert,
+}, app)
+  .listen(EXPRESS_PORT, () => {
+    console.log(`Server listening on port: ${EXPRESS_PORT}`);
+  });
