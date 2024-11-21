7936d70602b84821eb90606d8dfd6d18e7a54f52
raistlin-hess <raistlin-hess>
2024-11-20 20:28:11 -0500

    Hashed server password in init script

--- sql-files/z_createSampleUser.sql
+++ sql-files/z_createSampleUser.sql
@@ -1,5 +1,8 @@
 -- Refer to conf/groups.yml for all user group ids.
 
+-- Hash the default server user's password
+UPDATE login SET user_pass=MD5("p1") WHERE userid="s1";
+
 -- Create a user with Admin privileges
 INSERT INTO login (account_id, userid, user_pass, sex, email, group_id, character_slots) VALUES (2000000, "admin_user", MD5("password"), "M", "a@a.com", 99, 12);
 
