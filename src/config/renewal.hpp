91213c7eba77ad07aaa6c538f7a45adb95c41a3e
hessro-rathena <hessro-rathena>
2025-01-22 02:44:11 +0000

    mall changes

--- src/config/renewal.hpp
+++ src/config/renewal.hpp
@@ -34,7 +34,7 @@
 /// - FCT is NOT reduced by stats, reduced by equips or buffs.
 /// Example:
 ///  On a skill whos cast time is 10s, only 8s may be reduced. the other 2s are part of a FCT
-#define RENEWAL_CAST
+//#define RENEWAL_CAST
 
 /// Renewal drop rate algorithms
 /// (disable by commenting the line)
@@ -65,7 +65,7 @@
 /// - AGI has a greater factor in ASPD increase
 /// - there is a change in how skills/items give ASPD
 /// - some skill/item ASPD bonuses won't stack
-#define RENEWAL_ASPD
+//#define RENEWAL_ASPD
 
 /// Renewal stat calculations
 /// (disable by commenting the line)
